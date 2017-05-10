dc7ad54e8eb0f042cceed19d52f72388a13d077f
AllenLi65536 <allen79624@gmail.com>
2017-05-10 17:22:15 +0800

    Move RV parameters to TIBCORV

--- EDLib/Utility.cs
+++ EDLib/Utility.cs
@@ -10,42 +10,7 @@ namespace EDLib
     /// </summary>
     internal class NamespaceDoc { }
 
-    /// <summary>
-    /// TIBCO Rendezvous parameters
-    /// </summary>
-    public class RVParameters
-    {
-        /// <summary>
-        /// RV service parameter
-        /// </summary>
-        public readonly string service;
-        /// <summary>
-        /// RV network parameter
-        /// </summary>
-        public readonly string network;
-        /// <summary>
-        /// RV daemon parameter
-        /// </summary>
-        public readonly string daemon;
-        /// <summary>
-        /// RV topic parameter
-        /// </summary>
-        public readonly string topic;
-        /// <summary>
-        /// TIBCO Rendezvous parameters
-        /// </summary>
-        /// <param name="service">service</param>
-        /// <param name="network">network</param>
-        /// <param name="daemon">daemon</param>
-        /// <param name="topic">topic</param>
-        public RVParameters(string service, string network, string daemon, string topic) {
-            this.service = service;
-            this.network = network;
-            this.daemon = daemon;
-            this.topic = topic;
-        }
-
-    }
+   
     /// <summary>
     /// Global variables and parameters
     /// </summary>
@@ -59,31 +24,6 @@ namespace EDLib
         /// SQL server connection string of WMM3
         /// </summary>
         public static readonly string WMM3 = "Data Source=10.101.10.5;Initial Catalog=WMM3;User ID=hedgeuser;Password=hedgeuser";
-
-        /// <summary>
-        /// WMM3 Log RV parameters
-        /// </summary>
-        public static readonly RVParameters WMMLog = new RVParameters("9082", ";239.16.1.6", "10.60.0.128:7500", "TW.ED.WMM3.CLIENT.LOG");
-        /// <summary>
-        /// PM RV parameters
-        /// </summary>
-        public static readonly RVParameters PM = new RVParameters("9013", "172.31.2;239.16.1.72", "10.60.0.101:7500", "TW.WMM3.PM.PositionReport.>");
-        /// <summary>
-        /// Market liquidity RV parameters
-        /// </summary>
-        public static readonly RVParameters Liquidity = new RVParameters(null, "172.31.2;239.16.1.72", "10.60.0.128:7500", "MarketLiquidityInfo.*");
-        /// <summary>
-        /// TWSE quotes RV parameters
-        /// </summary>
-        public static readonly RVParameters TWSE = new RVParameters(null, "172.31.2;239.16.1.72", "10.60.0.128:7500", "TWSE.MarketDataSnapshotFullRefresh");
-        /// <summary>
-        /// Warrant execution report for calculating Slippage cost RV parameters
-        /// </summary>
-        public static readonly RVParameters Slippage = new RVParameters(null, "172.31.2;239.16.1.72", "10.60.0.101:7500", "TW.WMM3.SlippageCost.HedgeInfo.PROD");
-        /// <summary>
-        /// Warrant execution report for calculating Slippage cost RV parameters
-        /// </summary>
-        public static readonly RVParameters ExecutionReport = new RVParameters(null, "172.31.2;239.16.1.72", "10.60.0.129:7500", "TW.WMM3.FilledReportRelayService.ExecutionReport.PROD");
                
     }
 
