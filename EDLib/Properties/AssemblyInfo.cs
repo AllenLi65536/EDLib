f8cced51fd05bf2086d22c0b6e58820c38d2b993
AllenLi65536 <allen79624@gmail.com>
2017-06-03 09:50:32 +0800

    1.0.3

--- EDLib/Properties/AssemblyInfo.cs
+++ EDLib/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.2.0-beta")]
+[assembly: AssemblyInformationalVersion("1.0.3.0-beta")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
